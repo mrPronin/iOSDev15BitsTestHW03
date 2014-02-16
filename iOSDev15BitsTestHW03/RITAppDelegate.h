c15294a6171f6e54cf5fc56a9c27d76b28c0d528
Alexander Pronin <pronin.alx@gmail.com>
2014-02-16 17:26:09 +0200

    Initial Commit

---
+++ iOSDev15BitsTestHW03/RITAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RITAppDelegate.h
+//  iOSDev15BitsTestHW03
+//
+//  Created by Pronin Alexander on 16.02.14.
+//  Copyright (c) 2014 Pronin Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RITAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOSDev15BitsTestHW03Tests/iOSDev15BitsTestHW03Tests.m
@@ -0,0 +1,34 @@
+//
+//  iOSDev15BitsTestHW03Tests.m
+//  iOSDev15BitsTestHW03Tests
+//
+//  Created by Pronin Alexander on 16.02.14.
+//  Copyright (c) 2014 Pronin Alexander. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iOSDev15BitsTestHW03Tests : XCTestCase
+
+@end
+
+@implementation iOSDev15BitsTestHW03Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
